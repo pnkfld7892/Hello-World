a154f0aaff58978b8fdeacbc7c8646071ba344b8
pnkfld7892 <eschwarz0@gmail.com>
2016-02-09 20:02:27 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+          System.out.println("Seriously why are you looking at this program\nIt is really just a hello world");
+    }
+}
